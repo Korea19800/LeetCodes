091f4b6d881f8ed70b04bb3bad94e2cdda2214db
FrugalBoy <52956141+Korea19800@users.noreply.github.com>
2022-10-20 18:13:13 -0400

    Create leet100.cpp

---
+++ leet100.cpp
@@ -0,0 +1,38 @@
+//iterative
+
+  class Solution {
+  public:
+      bool isSameTree(TreeNode* p, TreeNode* q) {
+          queue<TreeNode *> queue;
+          queue.push(p);
+          queue.push(q);
+          while (queue.size()!=0){
+              TreeNode * q2=queue.front();
+              queue.pop();
+              TreeNode * q1=queue.front();
+              queue.pop();
+              if (q1==nullptr && q2==nullptr) continue;
+              if (q1==nullptr || q2==nullptr) return false;
+              if (q1->val!=q2->val) return false;
+              queue.push(q1->left);
+              queue.push(q2->left);
+              queue.push(q1->right);
+              queue.push(q2->right);
+          }
+          return true;
+      }
+  };
+  
+//recursive
+
+  class Solution {
+  public:
+      bool isSameTree(TreeNode* p, TreeNode* q) {
+          if(p==nullptr && q==nullptr) return true;
+          if(p==nullptr || q==nullptr) return false;
+          if(q->val!=p->val) return false;
+          return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+      }
+  };
+
+//https://leetcode.com/problems/same-tree/discuss/826280/recursive-and-iterativeeasy-understanding
